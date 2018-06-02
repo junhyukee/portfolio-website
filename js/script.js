b1694fee4ea658b6dc6d26689e0fe764237f6388
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-06-02 00:35:21 -0700

    added nav bar and smooth transitions

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	$('#slides').superslides({
 		animation: 'fade',
-		play: 4000,
+		play: 5000,
 		pagination: false
 	});
 
@@ -80,4 +80,29 @@ $(document).ready(function() {
 
 		return false;
 	});
+
+	$("#navigation li a").click(function(e) {
+		e.preventDefault();
+
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({scrollTop: targetPosition - 50}, "slow");
+	});
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation(){
+		const body = $("body");
+
+		if($(window).scrollTop() >= navTop){
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		} else {
+			body.css("padding-top", 0)
+			body.removeClass("fixedNav");
+		}
+	}
 });
